d5ceda3014f70f9cb894a929b445ded65c396e2c
Matt DeKrey <matt.dekrey@principlestudios.com>
2022-03-25 14:56:58 -0500

    Make credentials generation more specific

--- deployment/generate-credentials.ps1
+++ deployment/generate-credentials.ps1
@@ -2,9 +2,11 @@ param (
     [String]
     $subscription = '233e4023-8ea5-418c-b407-2ea999d54189',
     [String]
-    $azureResourceGroup = 'PrincipleK8s',
+    $aksResourceGroup = 'PrincipleK8s',
     [String]
-    $azureResourceGroup2 = 'PrincipleStudios',
+    $aksName = 'PrincipleToolsCluster',
+    [String]
+    $acrName = 'PrincipleStudios',
 
     [String]
     $appName = 'PrincipleTools-GitHub-Actions'
@@ -12,9 +14,28 @@ param (
 
 # see https://aka.ms/create-secrets-for-GitHub-workflows
 # version when written: https://github.com/Azure/actions-workflow-samples/blob/9a151eaa180e20378c308bcb24a3dcad7d49f22c/assets/create-secrets-for-GitHub-workflows.md
-$details = az ad sp create-for-rbac --name "$appName" --role contributor `
-    --scopes /subscriptions/$($subscription)/resourceGroups/$azureResourceGroup /subscriptions/$($subscription)/resourceGroups/$azureResourceGroup2 `
-    --sdk-auth | ConvertFrom-Json
+$details = az ad sp create-for-rbac --name "$appName" `
+    --skip-assignment `
+    --sdk-auth --only-show-errors | ConvertFrom-Json
+
+$objectId = az ad sp show --id 976a9a3f-8417-472f-ae53-0092ebb8c411 --query objectId -o tsv --only-show-errors
+
+$dump = az role assignment create `
+    --role AcrPush `
+    --assignee-principal-type ServicePrincipal `
+    --assignee-object-id $objectId `
+    --scope $(az acr show --name $acrName --query id -o tsv) `
+    --only-show-errors
+
+$dump = az role assignment create `
+    --role "Azure Kubernetes Service Cluster User Role" `
+    --assignee-principal-type ServicePrincipal `
+    --assignee-object-id $objectId `
+    --scope $(az aks show `
+        --resource-group $aksResourceGroup `
+        --name $aksName `
+        --query id -o tsv) `
+    --only-show-errors
 
 @{
     AZURE_CREDENTIALS = $details
