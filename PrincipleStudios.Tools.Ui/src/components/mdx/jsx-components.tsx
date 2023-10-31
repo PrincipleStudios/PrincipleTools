b721c336b668ff466fa5f7df4a9136a7ffff0a30
Matt DeKrey <matt.dekrey@principlestudios.com>
2023-10-31 10:44:36 -0500

    Initial pass at a branching strategy FAQ

--- PrincipleStudios.Tools.Ui/src/components/mdx/jsx-components.tsx
+++ PrincipleStudios.Tools.Ui/src/components/mdx/jsx-components.tsx
@@ -12,17 +12,6 @@ const infoFontTemplate = mergeStyles(<i className="font-info" />);
 
 export const components: import('mdx/types').MDXComponents = {
 	...Headings.byBaseNumber(1),
-	code: ({ children, className, ...props }) => (
-		<code
-			className={twMerge(
-				'bg-gray-200 inline-block border border-gray-400 rounded-sm',
-				className
-			)}
-			{...props}
-		>
-			{children}
-		</code>
-	),
 	p: mergeComponent(<p className="my-2" />),
 	table: ({ children, className, ...props }) => (
 		<div
@@ -59,6 +48,9 @@ export const components: import('mdx/types').MDXComponents = {
 		</blockquote>
 	),
 	strong: mergeComponent(<span className="font-bold" />),
+	pre: mergeComponent(
+		<pre className="bg-gray-800 p-4 overflow-hidden rounded-lg" />
+	),
 };
 
 export function getComponentsWithMaxHeading(
